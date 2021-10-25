edc70e0cb333ab1f89a2b09fe8fb96f80af58553
CaioBoa <caioob@al.insper.edu.br>
2021-10-25 16:22:20 -0300

    cria_pecas

--- EP2.py
+++ EP2.py
@@ -1,3 +1,17 @@
-a = 3
-print (a)
-print (4)
+import random
+
+def cria_pecas():
+    p = []
+    i = 0
+    while i <= 6:
+        j = 0
+        while j <= i:
+            e = [j,i]
+            p.append(e)
+            j += 1
+        i += 1
+    random.shuffle(p)
+    return p
+
+pecas = cria_pecas()
+print (pecas)
